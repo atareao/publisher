2d0b955f09c2891646f984586725e33511725297
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-02-22 07:29:14 +0100

    insert days

--- migrations/20220218074147_insert_days.up.sql
+++ migrations/20220218074147_insert_days.up.sql
@@ -1,7 +1,9 @@
-INSERT INTO days (name) VALUES ('lunes');
-INSERT INTO days (name) VALUES ('martes');
-INSERT INTO days (name) VALUES ('miercoles');
-INSERT INTO days (name) VALUES ('jueves');
-INSERT INTO days (name) VALUES ('viernes');
-INSERT INTO days (name) VALUES ('sabado');
-INSERT INTO days (name) VALUES ('domingo');
+INSERT INTO days (name)
+VALUES
+    ('lunes'),
+    ('martes'),
+    ('miercoles'),
+    ('jueves'),
+    ('viernes'),
+    ('sabado'),
+    ('domingo');
--- migrations/20220222061023_lists.up.sql
+++ migrations/20220222061023_lists.up.sql
@@ -1,7 +1,8 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS lists
 (
-    id      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name    TEXT NOT NULL,
-    reverse INTEGER NOT NULL DEFAULT 0
+    id         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    youtube_id TEXT NOT NULL,
+    name       TEXT NOT NULL,
+    reverse    INTEGER NOT NULL DEFAULT 0
 );
