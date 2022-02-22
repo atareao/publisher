1dc891e5a33d4d71296ef9382a978b90b614fcb6
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-02-22 07:38:57 +0100

    update lists

--- migrations/20220222062455_insert_lists.down.sql
+++ migrations/20220222062455_insert_lists.down.sql
@@ -1,1 +1,5 @@
 -- Add down migration script here
+DELETE FROM lists WHERE youtube_id='PL3lTiK2rXrUEU2RdRbTGuNr0DLOArfSsE';
+DELETE FROM lists WHERE youtube_id='PL3lTiK2rXrUEGdvNBfDup4el6v5lSf5ot';
+DELETE FROM lists WHERE youtube_id='PL3lTiK2rXrUELXR_w7y8c9it0fAkFJ600';
+DELETE FROM lists WHERE youtube_id='PL3lTiK2rXrUFdTzriDsmNCG28T8u7bhEd';
--- migrations/20220222062455_insert_lists.up.sql
+++ migrations/20220222062455_insert_lists.up.sql
@@ -4,4 +4,4 @@ VALUES
     ('PL3lTiK2rXrUEU2RdRbTGuNr0DLOArfSsE', 'aplicaciones-para-ubuntu', 0),
     ('PL3lTiK2rXrUEGdvNBfDup4el6v5lSf5ot', 'bot-para-telegram', 0),
     ('PL3lTiK2rXrUELXR_w7y8c9it0fAkFJ600', 'bspwm', 0),
-    ('PL3lTiK2rXrUFdTzriDsmNCG28T8u7bhEd', 'comandos-para-linux', 0),
+    ('PL3lTiK2rXrUFdTzriDsmNCG28T8u7bhEd', 'comandos-para-linux', 0);
