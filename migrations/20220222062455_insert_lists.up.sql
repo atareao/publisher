d60d69e26374828def0a24e113bf5de0adfbf48b
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-02-22 07:34:20 +0100

    insert lists

---
+++ migrations/20220222062455_insert_lists.up.sql
@@ -0,0 +1,7 @@
+-- Add up migration script here
+INSERT INTO lists (youtube_id, name, reverse)
+VALUES
+    ('PL3lTiK2rXrUEU2RdRbTGuNr0DLOArfSsE', 'aplicaciones-para-ubuntu', 0),
+    ('PL3lTiK2rXrUEGdvNBfDup4el6v5lSf5ot', 'bot-para-telegram', 0),
+    ('PL3lTiK2rXrUELXR_w7y8c9it0fAkFJ600', 'bspwm', 0),
+    ('PL3lTiK2rXrUFdTzriDsmNCG28T8u7bhEd', 'comandos-para-linux', 0),
