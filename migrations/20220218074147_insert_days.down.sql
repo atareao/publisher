72c7edc864e5c14e8a7e46fcd35def9e5408252a
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-02-22 07:17:27 +0100

    insert days

---
+++ migrations/20220218074147_insert_days.down.sql
@@ -0,0 +1,7 @@
+DELETE FROM days WHERE name='lunes';
+DELETE FROM days WHERE name='martes';
+DELETE FROM days WHERE name='miercoles';
+DELETE FROM days WHERE name='jueves';
+DELETE FROM days WHERE name='viernes';
+DELETE FROM days WHERE name='sabado';
+DELETE FROM days WHERE name='domingo';
---
+++ migrations/20220218074147_insert_days.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO days (name) VALUES ('lunes');
+INSERT INTO days (name) VALUES ('martes');
+INSERT INTO days (name) VALUES ('miercoles');
+INSERT INTO days (name) VALUES ('jueves');
+INSERT INTO days (name) VALUES ('viernes');
+INSERT INTO days (name) VALUES ('sabado');
+INSERT INTO days (name) VALUES ('domingo');
