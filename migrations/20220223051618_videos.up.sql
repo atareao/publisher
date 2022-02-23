b1cc193675ebc8ef15da31f2ba65939393298a7c
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-02-23 06:51:04 +0100

    add norder to videos

--- migrations/20220223051618_videos.up.sql
+++ migrations/20220223051618_videos.up.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS videos
 (
     youtube_id      TEXT PRIMARY KEY NOT NULL,
     youtube_list_id TEXT NOT NULL,
-    published       BOOLEAN NOT NULL DEFAULT FALSE
+    norder          INTEGER NOT NULL,
+    published       BOOLEAN NOT NULL DEFAULT FALSE,
+    UNIQUE(youtube_list_id, norder)
 );
--- src/video.rs
+++ src/video.rs
@@ -7,35 +7,37 @@ use serde::{Serialize, Deserialize};
 pub struct Video{
     pub youtube_id: String,
     pub youtube_list_id: String,
+    pub norder: i64,
     pub published: bool,
 }
 
 impl Video{
     pub async fn get_all(pool: web::Data<SqlitePool>) -> Result<Vec<Video>, Error>{
-        let videos = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, published FROM videos"#)
+        let videos = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, norder, published FROM videos"#)
             .fetch_all(pool.get_ref())
             .await?;
         Ok(videos)
     }
 
     pub async fn get_first(pool: web::Data<SqlitePool>) -> Result<Video, Error>{
-        let videos = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, published FROM videos LIMIT 1"#)
+        let videos = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, norder, published FROM videos LIMIT 1"#)
             .fetch_one(pool.get_ref())
             .await?;
         Ok(videos)
     }
 
     pub async fn get(pool: web::Data<SqlitePool>, youtube_id: &str) -> Result<Video, Error>{
-        let video = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, published FROM videos WHERE youtube_id=$1"#, youtube_id)
+        let video = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, norder, published FROM videos WHERE youtube_id=$1"#, youtube_id)
             .fetch_one(pool.get_ref())
             .await?;
         Ok(video)
     }
 
-    pub async fn new(pool: web::Data<SqlitePool>, youtube_id: &str, youtube_list_id: &str, published: bool) -> Result<Video, Error>{
-        query("INSERT INTO videos (youtube_id, youtube_list_id, published) VALUES (?, ?, ?);")
+    pub async fn new(pool: web::Data<SqlitePool>, youtube_id: &str, youtube_list_id: &str, norder: i64, published: bool) -> Result<Video, Error>{
+        query("INSERT INTO videos (youtube_id, youtube_list_id, norder, published) VALUES (?, ?, ?, ?);")
             .bind(youtube_id)
             .bind(youtube_list_id)
+            .bind(norder)
             .bind(published)
             .execute(pool.get_ref())
             .await?;
@@ -43,7 +45,7 @@ impl Video{
     }
 
     pub async fn delete(pool: web::Data<SqlitePool>, youtube_id: &str) -> Result<Video, Error>{
-        let video = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, published FROM videos WHERE youtube_id=$1"#, youtube_id)
+        let video = query_as!(Video, r#"SELECT youtube_id, youtube_list_id, norder, published FROM videos WHERE youtube_id=$1"#, youtube_id)
             .fetch_one(pool.get_ref())
             .await?;
         query("DELETE FROM videos WHERE youtube_id = ?")
@@ -53,9 +55,10 @@ impl Video{
         Ok(video)
     }
 
-    pub async fn update(pool: web::Data<SqlitePool>, youtube_id: &str, youtube_list_id: &str, published: bool) -> Result<Video, Error>{
-        query("UPDATE videos SET youtube_list_id=?, published=? WHERE youtube_id=?")
+    pub async fn update(pool: web::Data<SqlitePool>, youtube_id: &str, youtube_list_id: &str, norder: i64, published: bool) -> Result<Video, Error>{
+        query("UPDATE videos SET youtube_list_id=?, norder=?, published=? WHERE youtube_id=?")
             .bind(youtube_list_id)
+            .bind(norder)
             .bind(published)
             .bind(youtube_id)
             .execute(pool.get_ref())
